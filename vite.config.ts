a3cbca2b204c652d1680af13b38d2dc873edf8d0
Mostafa-Elsaeed <mostafaelsaeed1544@gmail.com>
2026-06-21 18:18:51 +0300

    WOrk Done: - fix server

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,16 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   server: {
     host: true,
