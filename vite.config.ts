5d6a0430efcfa854740db1f8330d0ef61d40b3aa
Mostafa-Elsaeed <mostafaelsaeed1544@gmail.com>
2026-06-23 15:16:27 +0300

    Work Done: - make port injectable

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,38 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const port = Number(process.env.PORT || env.PORT) || 1234;
 
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  return {
+    plugins: [
+      react(),
+      mode === "development" && componentTagger(),
+    ].filter(Boolean),
 
-  server: {
-    host: true,
-    port: Number(process.env.PORT) || 1234,
-    // strictPort: true,
-    allowedHosts: true,
-    watch: {
-      usePolling: true,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  preview: {
-    host: true,
-    port: Number(process.env.PORT) || 1234,
-    allowedHosts: true,
-  },
-}));
+
+    server: {
+      host: true,
+      port: port,
+      // strictPort: true,
+      allowedHosts: true,
+      watch: {
+        usePolling: true,
+      },
+    },
+    preview: {
+      host: true,
+      port: port,
+      allowedHosts: true,
+    },
+  };
+});
