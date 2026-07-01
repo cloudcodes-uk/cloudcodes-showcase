2c1ea8aa6e3ec72b8560cb7409f18484d546f2b8
Mostafa-Elsaeed <mostafaelsaeed1544@gmail.com>
2026-07-01 14:01:39 +0300

    Work Done: - update team info

--- src/dynamic/team/team.tsx
+++ src/dynamic/team/team.tsx
@@ -13,9 +13,17 @@ export const team: TeamMember[] = [
   {
     name: "Mohammed Madkour",
     role: "Backend Developer",
-    bio: "Backend Developer specializing in scalable web applications, cloud infrastructure, and AI solutions. Dedicated to building reliable software that helps businesses innovate and grow.",
-    image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=400&h=400&fit=crop&crop=face",
-    github: "#",
-    linkedin: "#"
+    bio: "Backend Developer focused on building scalable web applications and reliable server-side architecture using NestJS and PostgreSQL. Eager to innovate, he works closely with cloud infrastructure and AI solutions to optimize performance and ensure data integrity. Mohammed is dedicated to writing clean, efficient code that helps businesses build solid digital foundations and grow",
+    image: "/team_imgs/Mohamed_madkour_circle_img.png",
+    github: "https://github.com/abdul-latif",
+    linkedin: "https://www.linkedin.com/in/muhammad-madkour-46105b128"
+  },
+    {
+    name: "Mahmoud Elsayed",
+    role: "Frontend Developer",
+    bio: "Senior Frontend Developer specializing in building high-performance, scalable web applications with Next.js. Blending a deep understanding of modern cloud infrastructure with a passion for integrating cutting-edge AI solutions, he bridges the gap between complex architecture and seamless user experiences. Mahmoud is dedicated to engineering reliable, future-proof software that helps businesses innovate, scale, and grow",
+    image: "/team_imgs/Mahmoud_circle_img.png",
+    github: "https://github.com/Maahmoud-Elsayed",
+    linkedin: "https://www.linkedin.com/in/mahmoud-elsayed-9b1027267"
   },
 ];
